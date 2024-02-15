7ff621011087048e9094620378a95b4fc28b9a62
fernandouteca <eduardo.franco@uteca.edu.mx>
2024-02-14 19:47:09 -0600

    main - Movimiento simple
    Script movimiento simple arreglado, funciona y se mueve.

---
+++ PPV2/Assets/Scipts/MovimientoSimple.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class MovimientoSimple : MonoBehaviour
+{
+    public float velocidad = 5f;
+    Vector3 movimiento;
+
+    void Update()
+    {
+        // Obtener la entrada del teclado
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        // Calcular el vector de movimiento
+        //Vector3 movimiento = new Vector3 (movimientoHorizontal, 0f, movimientoVertical);
+        movimiento = new Vector3(movimientoHorizontal, movimientoVertical, 0f);
+
+        // Mover el objeto en la dirección calculada
+        transform.Translate (movimiento * velocidad * Time.deltaTime, Space.World);
+    }
+}
